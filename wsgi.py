84602ef1391fba7660ecb860d08167ece0c2f4df
Qasim Shah <qshah73@gmail.com>
2024-09-18 18:25:32 +0100

    update wsgi file

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,11 @@ import os
 # Set the path to your project directory
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Import the Flask app object from your run.py file
-from run import app as application
+# Import the create_app function from your app
+from app import create_app
 
-# The 'application' object will be picked up by Gunicorn
+# Create an instance of the app with the appropriate config
+app = create_app()
+
+# The 'app' object will be picked up by Gunicorn as 'application'
+application = app
