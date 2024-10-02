b88dbf805d422470e5a3cb4ef5cfaa1da7fa93c6
Qasim <qshah73@gmail.com>
2024-10-02 17:46:37 +0200

    Fixed wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,13 @@
 import sys
 import os
+import importlib
 
 # Set the path to your project directory
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Import the create_app function from your app
-from app import create_app
+# Dynamically import the create_app function from your app module
+app_module = importlib.import_module('app')
+create_app = getattr(app_module, 'create_app')
 
 # Create an instance of the app with the appropriate config
 app = create_app()
